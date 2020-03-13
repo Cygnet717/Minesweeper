f779ef2081a668c6315d8ebffc4a1b4eb8e8358f
Kathy Bradt <kathybradt717@gmail.com>
2020-03-13 14:25:29 -0500

    populating game board

--- script.js
+++ script.js
@@ -5,36 +5,54 @@ let mineField = [];
 function createMineField (size, numMines){
   for(let i=0; i<size; i++){
     mineField = mineField.concat('_')
-  }
+  };
   
   for(let j=0; j<numMines; j++){
     loadMineField(mineField);
-    
-  }
-  return mineField;
-}
+  };
+  return addNumbersToMineField(mineField);
+};
 
 function loadMineField(field){
   let randomLocation = Math.floor(Math.random()*Math.floor(field.length))
 
-    if(field[randomLocation]=== '**'){
-      loadMineField(field)
-    }else{
-      field[randomLocation]='**'
-      return field
-    }
-
-}
+  if(field[randomLocation]=== '**'){
+    loadMineField(field)
+  }else{
+    field[randomLocation]='**'
+    return field
+  }
+};
 
 function shuffle(randomArray) {
-            var tmp, current, top = randomArray.length;
-            if(top) while(--top) {
-                current = Math.floor(Math.random() * (top + 1));
-                tmp = randomArray[current];
-                randomArray[current] = randomArray[top];
-                randomArray[top] = tmp;
-            };
-        return randomArray;
-    }
+  var tmp, current, top = randomArray.length;
+  if(top) while(--top) {
+    current = Math.floor(Math.random() * (top + 1));
+    tmp = randomArray[current];
+    randomArray[current] = randomArray[top];
+    randomArray[top] = tmp;
+  };
+  return randomArray;
+};
 
-console.log(createMineField(25, 3))
+
+
+function addNumbersToMineField(mineField){
+  for(let i=0; i<mineField.length; i++){
+    let count =0;
+    if()
+  }
+};
+
+//populate game borad with numbers and bombs
+function fillGameBox(mineField){
+  let gameBoxContents = '';
+  for (let i=0; i<mineField.length; i++){
+    gameBoxContents = gameBoxContents.concat(`<div class="grid-item">${mineField[i]}</div>`)
+  }
+  $('.gamebox').append(gameBoxContents)
+}
+
+createMineField(20, 5);
+
+fillGameBox(mineField);
