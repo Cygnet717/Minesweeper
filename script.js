259983c88b18a5a22b39b2806759f29dcf5c326b
Kathy Bradt <kathybradt717@gmail.com>
2020-03-13 15:41:57 -0500

    half number adding function done

--- script.js
+++ script.js
@@ -44,31 +44,55 @@ function cutMineFieldIntoRows(mineField){
     row = row.concat(mineField.slice(i, i+4))
     mineFieldRows[rowCount]=row
     rowCount++
-    console.log(rowCount)
   }
- //mineField = mineFieldRows;
   
-  return console.log(mineFieldRows)
+  return addNumbersToMineField(mineFieldRows)
 }
 
 
 
-/*function addNumbersToMineField(mineField){
-  for(let i=0; i<mineField.length; i++){
-    let count =0;
-    if()
+function addNumbersToMineField(rowed){
+  for(let i=0; i<rowed.length; i++){
+    let prev;
+    let curr = rowed[i];
+    let next;
+    if(rowed[i-1]){
+      prev = rowed[i-1]
+    } else {
+      prev = null
+    }
+
+    if(rowed[i+1]){
+      next=rowed[i+1]
+    } else {
+      next = null
+    }
+
+    for(let j=0; j<curr.length; j++){
+      let count =0;
+      if()
+        //check all adjacent locations
+      
+        //add to count for each bomb in adjacent locations
+        //let i[j]=count;
+        //return;
+      
+    }
   }
-};*/
+
+  return console.log(rowed)
+};
 
 //populate game borad with numbers and bombs
 function fillGameBox(mineField){
-  let gameBoxContents = '';
+  console.log(mineField)
+  /*let gameBoxContents = '';
   for (let i=0; i<mineField.length; i++){
     gameBoxContents = gameBoxContents.concat(`<div class="grid-item">${mineField[i]}</div>`)
   }
-  $('.gamebox').append(gameBoxContents)
+  $('.gamebox').append(gameBoxContents)*/
 }
 
 createMineField(20, 5);
 
-fillGameBox(mineField);
+//fillGameBox(mineField);
